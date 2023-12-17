67675bc5f9455a4448324ec767a6af01a11d4980
Rundredoffi <contact.rundred@gmail.com>
2023-12-17 17:13:28 +0100

    Updating code
    Wrong version uploaded :
    1.7 => 2.1

--- Capteur CO2 - Prod_final.ino
+++ Capteur CO2 - Prod_final.ino
@@ -1,72 +1,71 @@
+//Configuration :
+//Servo : 8
+//Sensor : I2C
+//écran LCD : I2C
+
+//Bibliothèques à installer pour le fonctionnement :
+// DHT by Adafruit
+// Grove RGB
+// SGP 30
+ 
 //Bibliothèques de pour le capteur de CO2
 #include <Wire.h>
 #include "Adafruit_SGP30.h"
-
-int var;// Variable pour avoir la valeur du capteur de CO2
 Adafruit_SGP30 sgp;
 
 //Bibliothèques ServoMoteur
 #include <Servo.h>
 Servo myservo;  // create servo object to control a servo
-// Variable Servomoteur
-int servo_pin = 0;  // analog pin used to connect the potentiometer
-int val;    // variable to read the value from the analog pin
+int servo_pin = 8;  // Branchement du servo
 
-//LEDS
-#define redLed 9
-#define greenLed 11
+//Bibliothèques écran LCD
+#include "rgb_lcd.h" // inclure la bibliothèque pour l'écran LCD
+rgb_lcd lcd; // Création d'un objet lcd, facilitation du programme
+
+//Bibliothèques DHT
+#include <DHT.h>
+#define branchement 6    // La ligne de données de notre DHT22 sera branchée sur la pin D6 de notre Arduino
+#define ref DHT22             // Spécification du type de DHT utilisé (ici, ce sera un DHT22, comme indiqué)
+DHT dht(branchement, ref); // Instanciation de la librairie DHT
+
 
 void setup() {
+  //Setup du servomoteur
+  myservo.attach(servo_pin);  //Définition du servo
 //Setup du capteur de CO2, sur un port I2C Grove
   Serial.begin(115200);
-  if (! sgp.begin()){ // Recherche du capteur de CO2
-    Serial.println("Le capteur n'a pas été trouvé pensé à le mettre sur un port I2C !");
-    while (1);
-    }
-  Serial.print("Capteur trouvé sur un port I2C !");
+  sgp.begin();
   delay(200);
-  }
-  Serial.print("Capteur trouvé sur un port I2C !");
-int counter = 0;
-// Fin du setup capteur de CO2
-//Setup du servomoteur
-  myservo.attach(servo_pin);  //Permet de définir le port de position ServoMoteur
-//Fin du setup servomoteur
-//Setup LEDS
-  pinMode(redLed,OUTPUT);
-  pinMode(greenLed,OUTPUT);
-//Fin du setup LEDS
+//Setup écran LCD
+lcd.begin(16, 2);   // Détermination nombre : collonnes, lignes
+lcd.setRGB(255,229,0); // Configuration couleur écran LCD
+lcd.setCursor(2,0);lcd.print("Capteur de CO2 "); //Première partie du message d'accueil
+lcd.setCursor(2,1);lcd.print("Je demarre... ");//Deuxième partie du message d'accueil
+delay(2000);
+//Setup DHT22 (Température)
+  dht.begin();
 }
 
 void loop() {
-  //Partie Capteur Arduino, requete d'information
-if (! sgp.IAQmeasure()) {
-    Serial.println("Erreur de mesure");
-    return;
+    sgp.IAQmeasure();
+  if (sgp.eCO2 < 800) {
+  lcd.setRGB(0,255,0);
+  sgp.IAQmeasure(); // Première mesure
+  lcd.setCursor(1,0);lcd.print("CO2 ");lcd.setCursor(5,0);lcd.print(sgp.eCO2);lcd.print(" ppm");//Affichage taux de CO2  sur le LCD
+  lcd.setCursor(1,1);lcd.print("Temp ");lcd.setCursor(6,1);lcd.print((float)dht.readTemperature());lcd.print(" c");
+  delay(1000);
+  lcd.clear(); // Affichage température sur le LCD
   }
-  Serial.println("");
-  Serial.print("Taux de CO2 "); Serial.print(sgp.eCO2); Serial.println(" ppm");
-  var = sgp.eCO2;
 
-  if (! sgp.IAQmeasureRaw()) {
-    Serial.println("Erreur de mesure");
-    return;
-  }
-  //Fin requete CO2
-  //Taux inférieur de CO2
-    while (var > 799) {
-    digitalWrite( greenLed,   HIGH  ) ; // Led Rouge ON
-    delay(15);                           // waits for the servo to get there
-  }
-  //Lorsque le taux de CO2 est trop haut
-  while (var < 799) {
-    digitalWrite( redLed,   HIGH  ) ; // Led Rouge ON
-    myservo.write(180);                  // sets the servo position according to the scaled value
-    delay(15);                           // waits for the servo to get there
-  }
-    while (var < 799) {
-    digitalWrite( redLed,   HIGH  ) ; // Led Rouge ON
-    myservo.write(180);                  // sets the servo position according to the scaled value
-    delay(15);                           // waits for the servo to get there
+  //Si taux supérieur de CO2
+    else {
+  lcd.setRGB(255,0,0);
+  myservo.write(180);                  // sets the servo position according to the scaled value
+  delay(15);                           // waits for the servo to get there
+  sgp.IAQmeasure();
+  lcd.setCursor(1,0);lcd.print("CO2 ");lcd.setCursor(5,0);lcd.print(sgp.eCO2);lcd.print(" ppm"); //Affichage taux de CO2  sur le LCD
+  lcd.setCursor(1,1);lcd.print("Temp ");lcd.setCursor(6,1);lcd.print((float)dht.readTemperature());lcd.print(" c");delay(500);lcd.clear(); // Affichage température sur le LCD
+  delay(1000);
+  lcd.clear();
   }
 }
--- README.md
+++ README.md
@@ -4,7 +4,6 @@ Bienvenue dans le projet Capteur CO2 ! Ce projet vise à mesurer les niveaux de 
 
 ## Table des matières
 
-- [Aperçu](#aperçu)
 - [Matériel utilisé](#matériel-utilisé)
 - [Configuration](#configuration)
 - [Installation](#installation)
@@ -12,19 +11,41 @@ Bienvenue dans le projet Capteur CO2 ! Ce projet vise à mesurer les niveaux de 
 - [Contributions](#contributions)
 - [Licence](#licence)
 
-## Aperçu
-
-[Insérez ici une brève description ou une image de votre projet en action.]
-
 ## Matériel utilisé
 
-- **Capteur de CO2:** [Modèle du capteur utilisé]
-- **Microcontrôleur:** [Modèle du microcontrôleur utilisé]
-- **Autres composants:** [Liste des autres composants nécessaires]
+- **Capteur de CO2:** [SGP 30 Grove](https://www.gotronic.fr/art-capteur-de-gaz-sgp30-grove-101020512-28427.htm)
+- **Microcontrôleur:** [Arduino Uno](https://store.arduino.cc/products/arduino-uno-rev3)
+- **Afficheur:** [Écran LCD Grove](https://www.gotronic.fr/art-afficheur-lcd-2x16-grove-104030001-21308.htm)
+- **Autres composants:** [ServoMoteur](https://www.gotronic.fr/art-servomoteur-grove-316010005-20640.htm)
 
+````markdown
 ## Configuration
 
-[Expliquez ici comment configurer le matériel pour votre projet. Incluez des informations sur les connexions, les broches, etc.]
+Avant de commencer, assurez-vous d'avoir tous les composants matériels nécessaires pour le projet. Voici comment configurer chaque composant en tenant compte des ports de connexion spécifiés :
+
+### Capteur de CO2 (SGP 30 Grove)
+
+1. Connectez le capteur de CO2 au port I2C sur le microcontrôleur.
+2. Assurez-vous que le capteur est alimenté correctement selon les spécifications du fabricant.
+3. Consultez la documentation du capteur pour obtenir des détails sur la communication avec le microcontrôleur.
+
+### Microcontrôleur (Arduino Uno)
+
+1. Assurez-vous d'avoir le logiciel Arduino IDE installé sur votre ordinateur.
+2. Connectez l'Arduino Uno à votre ordinateur via un câble USB.
+3. Téléversez le code du projet sur l'Arduino en utilisant l'IDE Arduino.
+
+### Afficheur LCD (Écran LCD Grove)
+
+1. Connectez l'afficheur LCD au port I2C sur le microcontrôleur.
+2. Assurez-vous que l'afficheur est alimenté correctement selon les spécifications du fabricant.
+3. Consultez la documentation de l'afficheur pour obtenir des détails sur l'initialisation et l'affichage de données.
+
+### ServoMoteur
+
+1. Connectez le ServoMoteur au port 8 sur le microcontrôleur.
+2. Assurez-vous que le ServoMoteur est alimenté correctement selon les spécifications du fabricant.
+3. Consultez la documentation du ServoMoteur pour obtenir des détails sur son utilisation.
 
 ## Installation
 
@@ -32,3 +53,15 @@ Bienvenue dans le projet Capteur CO2 ! Ce projet vise à mesurer les niveaux de 
    ```bash
    git clone https://github.com/votre-utilisateur/votre-projet.git
    ```
+2. Naviguez vers le répertoire du projet.
+   cd votre-projet
+
+3. Installez les bibliothèques nécessaires en utilisant l'IDE Arduino. Assurez-vous d'installer les bibliothèques suivantes :
+
+   - DHT by Adafruit
+   - Grove RGB
+   - SGP 30
+
+4. Téléversez le code sur l'Arduino à l'aide de l'IDE Arduino.
+   Assurez-vous d'ajuster ces instructions en fonction des spécificités de votre projet et des composants réels que vous utilisez.
+````
